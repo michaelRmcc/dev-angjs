863ef7a4a498aee7b6041c89494abe02c8693a9b
MichaelR <mccmichaelr@gmail.com>
2016-09-28 09:35:09 -0400

    added functionality to input items/check number items entered.

--- mod-sol-1/js/app.js
+++ mod-sol-1/js/app.js
@@ -1,13 +1,21 @@
 (function () {
-  use strict;
+  'use strict';
 
   angular.module('LunchCheck',[])
   .controller('LunchCheckController', LunchCheckController);
 
-  :LunchCheckController.$inject = ['$scope'];
+  LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
     $scope.lunch = "";
+    $scope.howMuch = 0;
+    
+    $scope.lunchItems = function() {
+      var r = $scope.lunch.split(",").length;
+      console.log( ("lunch split length is:" +  r) );
+      $scope.howMuch = r;
+    };
 
   }
 
-})
+
+}) ();
